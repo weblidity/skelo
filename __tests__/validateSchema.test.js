12e51d471a9fd06e8da3f79870408266955f640f
Ion Gireada <shytiger@yahoo.com>
2024-11-25 22:39:01 +0200

    * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test.js): remove unnecessary empty lines * test(validateSchema.test

--- __tests__/validateSchema.test.js
+++ __tests__/validateSchema.test.js
@@ -25,8 +25,6 @@ describe('validateSchema', () => {
         expect(consoleErrorSpy).toHaveBeenCalledWith('Schema validation error: Test Error');
         consoleErrorSpy.mockRestore();
     });
-
-
 });
 
 describe('validateAndSetSchema', () => {
@@ -46,15 +44,9 @@ describe('validateAndSetSchema', () => {
 
         consoleErrorSpy.mockRestore();
     });
-
-
 });
 
-
-
-
 describe('readAndValidateSchema', () => {
-
     it('should return default schema if schema file does not exist', () => {
         const consoleSpy = jest.spyOn(console, 'error');
         fs.readFileSync.mockImplementation(() => {
@@ -65,24 +57,19 @@ describe('readAndValidateSchema', () => {
         consoleSpy.mockRestore();
     });
 
-
     it('should return the schema path if valid', () => {
         fs.readFileSync.mockReturnValue('{"type": "object"}');
         expect(readAndValidateSchema('valid/path', 'default/path')).toBe('valid/path');
     });
 
     it('should return the default schema if invalid JSON', () => {
-
         const consoleSpy = jest.spyOn(console, 'error');
         fs.readFileSync.mockReturnValue('{invalid json}');
 
         expect(readAndValidateSchema('path/to/invalid.json', 'default/schema.json')).toBe('default/schema.json');
         expect(consoleSpy).toHaveBeenCalledWith('Error parsing or validating JSON schema: path/to/invalid.json. Using default schema: default/schema.json');
         consoleSpy.mockRestore();
-
-
     });
-
 
     it('should return default schema if schema validation fails', () => {
         const consoleErrorSpy = jest.spyOn(console, 'error');
@@ -93,14 +80,8 @@ describe('readAndValidateSchema', () => {
         expect(consoleErrorSpy).toHaveBeenCalledWith('Error parsing or validating JSON schema: invalid/schema. Using default schema: default/schema');
 
         consoleErrorSpy.mockRestore();
-
-
     });
-
-
 });
-
-
 
 describe('useDefaultSchemaIfNeeded', () => {
     const defaultSchema = 'default/schema.json';
@@ -108,7 +89,6 @@ describe('useDefaultSchemaIfNeeded', () => {
     afterEach(() => {
         jest.clearAllMocks();
     });
-
 
     it('should throw an error if defaultSchemaLocation is invalid', () => {
         expect(() => useDefaultSchemaIfNeeded({}, null)).toThrow('Invalid default schema location provided.');
@@ -118,41 +98,29 @@ describe('useDefaultSchemaIfNeeded', () => {
     it('should throw error if defaultSchemaLocation does not exist', () => {
         fs.existsSync.mockReturnValue(false);
         expect(() => useDefaultSchemaIfNeeded({}, defaultSchema)).toThrow('Default schema location is invalid: default/schema.json');
-
     });
 
     it('should return default schema if options object is invalid and log to console', () => {
-
         const consoleSpy = jest.spyOn(console, 'error');
         fs.existsSync.mockReturnValue(true);
-
 
         expect(useDefaultSchemaIfNeeded(null, defaultSchema).schema).toBe(defaultSchema);
         expect(consoleSpy).toHaveBeenCalledWith(expect.stringContaining('Invalid options object. Using default schema.'));
 
         consoleSpy.mockRestore();
-
-
     });
 
     it('should use default schema if options.schema is invalid', () => {
-
         const consoleErrorSpy = jest.spyOn(console, 'error');
         fs.existsSync.mockReturnValueOnce(true);
         fs.existsSync.mockReturnValueOnce(false)
 
         const options = { schema: 'invalid/schema.json' };
 
-
         expect(useDefaultSchemaIfNeeded(options, defaultSchema).schema).toBe(defaultSchema);
-
 
         expect(consoleErrorSpy).toHaveBeenCalledWith('Schema file not found: invalid/schema.json. Using default schema: default/schema.json');
         consoleErrorSpy.mockRestore();
-
-
-
-
     });
 
     it('should correctly validate using functions', () => {
@@ -164,7 +132,6 @@ describe('useDefaultSchemaIfNeeded', () => {
         expect(result.schema).toBe('valid/schema.json');
     });
 
-
     it('should return options with default schema if specified schema is invalid json', () => {
         fs.existsSync.mockReturnValueOnce(true);
         fs.readFileSync.mockReturnValueOnce('{invalid}');
@@ -173,10 +140,5 @@ describe('useDefaultSchemaIfNeeded', () => {
 
         const result = useDefaultSchemaIfNeeded(options, 'default/schema.json');
         expect(result.schema).toEqual('default/schema.json')
-
     })
-
-
-
-
 });
